67284c94051450863d3da427a9b73fc729149da9
Solomon <gbolahanbalogun5@gmail.com>
2020-02-23 15:50:20 +0100

    Refactor code for displaying key operators on button click

--- script.js
+++ script.js
@@ -1,30 +1,26 @@
 const display = document.querySelector(".calculator_display");
 const numbers = document.querySelectorAll('.number');
-const numberArray = [7, 8, 9, 4, 5, 6, 1, 2, 3, 0]
+const keyOperators = document.querySelectorAll('.key-operator');
+const numberArray = [7, 8, 9, 4, 5, 6, 1, 2, 3, 0];
+const operatorArray = ['+', '-', '*', '/'];
 
 for(let i = 0; i < numbers.length; i++) {
 	numbers[i].addEventListener('click', function() {
-		display.textContent += `${numberArray[i]}`
+		display.textContent += `${numberArray[i]}`;
+	})
+}
+
+for(let i = 0; i < keyOperators.length; i++) {
+	keyOperators[i].addEventListener('click', function() {
+		display.textContent += `${operatorArray[i]}`;
 	})
 }
 
 let decimal = document.querySelector(".decimal")
-let add = document.querySelector(".add")
-let subtract = document.querySelector(".sub")
-let multiply = document.querySelector(".times")
-let divide = document.querySelector(".div")
 let clear = document.querySelector(".clearall")
 let answer = document.querySelector(".key-equal")
 
 decimal.addEventListener("click", function () {display.textContent += '.'})
-
-add.addEventListener("click", function () {display.textContent += '+'})
-
-subtract.addEventListener("click", function () {display.textContent += '-'})
-
-multiply.addEventListener("click", function () {display.textContent += '*'})
-
-divide.addEventListener("click", function () {display.textContent += '/'})
 
 clear.addEventListener("click", function () {display.textContent = ''})
 
