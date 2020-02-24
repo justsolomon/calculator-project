63b0e3ad59ef35ad4f2f8d63e38d1f720de83f84
Solomon <gbolahanbalogun5@gmail.com>
2020-02-24 18:18:40 +0100

    Add code to update textcontent of answerDisplay when enter key is clicked

--- script.js
+++ script.js
@@ -38,5 +38,6 @@ clear.addEventListener("click", function () {display.textContent = ''})
 deleteEntry.addEventListener('click', function () {display.textContent = display.textContent.slice(0, -1)})
 
 answer.addEventListener("click", function() {
+	answerDisplay.textContent = display.textContent;
 	display.textContent = eval(display.textContent)
 })
